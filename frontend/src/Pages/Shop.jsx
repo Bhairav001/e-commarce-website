f9790238e914fd08372d0fa3d7e1796a4be24746
Bhairav <bhairavgotam066@gmail.com>
2023-10-15 18:32:24 +0530

    added womens page

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import Populer from '../components/Populer/Populer'
 
 const Shop = () => {
   return (
     <div>
         <Hero/>
+        <Populer/>
     </div>
   )
 }
