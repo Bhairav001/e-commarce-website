08eebed4354d44f3b88315188d0799dc6ba0603d
Bhairav <bhairavgotam066@gmail.com>
2023-10-16 21:30:02 +0530

    added frontend update

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import Populer from '../components/Populer/Populer'
+import Offer from '../components/Offers/Offer'
 
 const Shop = () => {
   return (
     <div>
         <Hero/>
         <Populer/>
+        <Offer/>
     </div>
   )
 }
