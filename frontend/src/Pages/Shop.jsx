33214792733cc44440cc9d2db48adb58b1e26c2f
Bhairav <bhairavgotam066@gmail.com>
2023-10-18 10:16:08 +0530

    added frontend collection part

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero/Hero'
 import Populer from '../components/Populer/Populer'
 import Offer from '../components/Offers/Offer'
 import NewCollection from '../components/NewCollections/NewCollection'
+import NewsLetter from '../components/NewsLetter/NewsLetter'
 
 const Shop = () => {
   return (
@@ -11,6 +12,7 @@ const Shop = () => {
         <Populer/>
         <Offer/>
         <NewCollection/>
+        <NewsLetter/>
     </div>
   )
 }
